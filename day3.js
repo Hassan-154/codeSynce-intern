1be5421f0d285c0c2bea4d8d82f90a3c40af40bd
Hassan-154 <83923230+Hassan-154@users.noreply.github.com>
2023-10-12 19:14:23 +0500

    code push

--- day3.js
+++ day3.js
@@ -1,14 +1,14 @@
 //lets crate a filter array
-const numbers= [34, 12, 33, 10 ];
+const numbers = [34, 12, 33, 10];
 let arrayFilter = numbers.filter(fun);
-function fun(value, index, completeArray){
-   console.log("array element: "+ value);
-   console.log("array index: "+ index);
-   console.log("complete array: "+ completeArray);
-   //apply condition to filer the array
-   return value < 14;
+function fun(value, index, completeArray) {
+  console.log("array element: " + value);
+  console.log("array index: " + index);
+  console.log("complete array: " + completeArray);
+  //apply condition to filer the array
+  return value < 14;
 }
-console.log("filer number return: " + arrayFilter)
+console.log("filer number return: " + arrayFilter);
 
 // the difference between every array and some array
 // the every array demand to check every element to pass the condition
@@ -16,109 +16,119 @@ console.log("filer number return: " + arrayFilter)
 
 // create every array
 let every = numbers.every(everyFun);
-function everyFun(value){
+function everyFun(value) {
   return value < 60;
 }
 console.log("every array return value: " + every);
 
 // create some array
 let some = numbers.some(someFun);
-function someFun(value){
+function someFun(value) {
   return value < 12;
 }
 console.log("some array return value: " + some);
 
 // create every array with the other type of element
-const names = ['hassan', 'hassan', 'hassan'];
+const names = ["hassan", "hassan", "hassan"];
 let everyName = names.every(everyNameFun);
-function everyNameFun(name, index){
+function everyNameFun(name, index) {
   return name == "hassan";
 }
-console.log("every array return name:" + everyName );
+console.log("every array return name:" + everyName);
 
 //create some array with the other type of element
 let someName = names.some(someNameFun);
-function someNameFun(name){
-  return name == "hassan"
+function someNameFun(name) {
+  return name == "hassan";
 }
-console.log("some array return name: " + someName)
+console.log("some array return name: " + someName);
 
 // find array method return the first element that pass the test4
 let find = numbers.find(findFun);
-function findFun(value){
+function findFun(value) {
   return value > 18;
 }
-console.log("return the find value: " + find );
+console.log("return the find value: " + find);
 
 // find array index return the first element that pass the test
 let findIndex = numbers.findIndex(findFunIndex);
-function findFunIndex(value){
+function findFunIndex(value) {
   return value < 12;
 }
-console.log("return the find index value: " + findIndex );
+console.log("return the find index value: " + findIndex);
 
-//array form 
+//array form
 let arrayform = Array.from("83754949");
 console.log(arrayform);
 
 //the keys  method in array
 let saveKey = numbers.keys();
-for(let x of saveKey){
-  console.log("key method: " + x)
+for (let x of saveKey) {
+  console.log("key method: " + x);
 }
 
 //the entries  method in array
 let arrayEntries = numbers.entries();
-for(let x of arrayEntries){
-  console.log("the entries method: " + x)
+for (let x of arrayEntries) {
+  console.log("the entries method: " + x);
 }
 
 // hoisting
 // we can do that in var but we cant do in let and const
 x = 5; // Assign 5 to x
 var x; // Declare x
-console.log(x)
+console.log(x);
 //we can also do that in var ofCourse but we cant do this in let and const
 var x; // Declare x
 x = 5; // Assign 5 to x
-console.log(x)
+console.log(x);
 
 //now the var will be undefined because we console before
-console.log(y)
-var y = 7;  // Initialize y
-
+console.log(y);
+var y = 7; // Initialize y
 
 //bigint data type
 
 //explicit type casting
-console.log(23)
-console.log("here is the number " + 23 + ".. .")
+console.log(23);
+console.log("here is the number " + 23 + ".. .");
 
 //implicit type casting5684
 let num = "23";
-console.log(String(num));
+console.log(Number(num));
 
 //loops and iteration
 
 //for loop
 for (let i = 0; i < numbers.length; i++) {
-  console.log("array number: "+numbers[i])
+  console.log("array number: " + numbers[i]);
 }
 //for in loop
-const person = {fname:"John", lname:"Doe", age:25}; 
-for(let p in person){
-  console.log("person data: " + person[p])
+const person = { fname: "John", lname: "Doe", age: 25 };
+for (let p in person) {
+  console.log("person data: " + person[p]);
 }
 
 //the for of loop
 //the x of number directly show the values instead of  index
-for (let x of numbers){
+for (let x of numbers) {
   console.log("for off loop: " + x);
 }
 
 //do while loop
 let i = 0;
 while (i < 10) {
-  console.log("the number is:" + i)
+  console.log("the number is:" + i);
   i++;
 }
+
+function isSumPossible(target, arr) {
+  // return true if possible
+  return true;
+}
+
+console.log(isSumPossible(7, [1, 3, 4]));
+
+function removeRepeatedValues(arr) {}
+
+console.log(removeRepeatedValues([2, 3, 13, 42, 1, 2, 42]));
--- day4.js
+++ day4.js
@@ -1,124 +1,129 @@
-//lets crate a filter array
-const numbers= [34, 12, 33, 10 ];
-let arrayFilter = numbers.filter(fun);
-function fun(value, index, completeArray){
-   console.log("array element: "+ value);
-   console.log("array index: "+ index);
-   console.log("complete array: "+ completeArray);
-   //apply condition to filer the array
-   return value < 14;
-}
-console.log("filer number return: " + arrayFilter)
+//primitive and non primitive
+//primitive
+let x = 9;
+let b = 0;
+//changing by value
+b = x;
+console.log("primitive data type value: " + b);
+//non primitive
+const obj = { name: "hassan", age: "24" };
+//changing by reference
+obj.name = "hamza";
+console.log(obj.name);
+//changing by value will take own memory
+//changing by reference will not not take memory
 
-// the difference between every array and some array
-// the every array demand to check every element to pass the condition
-// but the some array demand if even only one array element pass the test
+//object.is
+Object.is(5, 5); // true
+Object.is("5", 5); // false
+Object.is(0, -0); // false
+Object.is(NaN, NaN); // true
+NaN === NaN; // false
 
-// create every array
-let every = numbers.every(everyFun);
-function everyFun(value){
-  return value < 60;
-}
-console.log("every array return value: " + every);
+//example related to object
+const obj1 = { name: "hassan" };
+const obj2 = { name: "hassan" };
+let findAns = Object.is(true, true);
+console.log("find object: " + findAns);
 
-// create some array
-let some = numbers.some(someFun);
-function someFun(value){
-  return value < 12;
-}
-console.log("some array return value: " + some);
-
-// create every array with the other type of element
-const names = ['hassan', 'hassan', 'hassan'];
-let everyName = names.every(everyNameFun);
-function everyNameFun(name, index){
-  return name == "hassan";
-}
-console.log("every array return name:" + everyName );
-
-//create some array with the other type of element
-let someName = names.some(someNameFun);
-function someNameFun(name){
-  return name == "hassan"
-}
-console.log("some array return name: " + someName)
-
-// find array method return the first element that pass the test4
-let find = numbers.find(findFun);
-function findFun(value){
-  return value > 18;
-}
-console.log("return the find value: " + find );
-
-// find array index return the first element that pass the test
-let findIndex = numbers.findIndex(findFunIndex);
-function findFunIndex(value){
-  return value < 12;
-}
-console.log("return the find index value: " + findIndex );
-
-//array form 
-let arrayform = Array.from("83754949");
-console.log(arrayform);
-
-//the keys  method in array
-let saveKey = numbers.keys();
-for(let x of saveKey){
-  console.log("key method: " + x)
-}
-
-//the entries  method in array
-let arrayEntries = numbers.entries();
-for(let x of arrayEntries){
-  console.log("the entries method: " + x)
-}
-
-// hoisting
-// we can do that in var but we cant do in let and const
-x = 5; // Assign 5 to x
-var x; // Declare x
-console.log(x)
-//we can also do that in var ofCourse but we cant do this in let and const
-var x; // Declare x
-x = 5; // Assign 5 to x
-console.log(x)
-
-//now the var will be undefined because we console before
-console.log(y)
-var y = 7;  // Initialize y
-
-
-//bigint data type
-
-//explicit type casting
-console.log(23)
-console.log("here is the number " + 23 + ".. .")
-
-//implicit type casting5684
-let num = "23";
-console.log(String(num));
-
-//loops and iteration
-
-//for loop
-for (let i = 0; i < numbers.length; i++) {
-  console.log("array number: "+numbers[i])
-}
-//for in loop
-const person = {fname:"John", lname:"Doe", age:25}; 
-for(let p in person){
-  console.log("person data: " + person[p])
-}
-
-//the for of loop
-//the x of number directly show the values instead of  index
-for (let x of numbers){
-  console.log("for off loop: " + x);
+//next loop statements
+for (let i = 0; i < 10; i++) {
+  if (i === 3) {
+    console.log("here is the value: " + i);
+    break;
+  }
+  console.log("loop is running: " + i);
 }
 
 //do while loop
 let i = 0;
 while (i < 10) {
-  console.log("the number is:" + i)
+  console.log("the number is:" + i);
   i++;
 }
+
+//do while loop
+// it will run one time even if the condition is false
+let no = 1;
+do {
+  console.log("running loop: " + no);
+  no++;
+} while (no < 5);
+
+const time = 35;
+if (time < 10) {
+  console.log("if");
+} else if (time < 30) {
+  console.log("else if");
+} else {
+  console.log("else");
+}
+
+//switch statement
+var day = "Monday";
+
+switch (day) {
+  case "Monday":
+    console.log("It's the start of the workweek.");
+    break;
+  case "Friday":
+    console.log("It's almost the weekend!");
+    break;
+  default:
+    console.log("It's a regular day.");
+}
+
+let a = 10;
+let c = "10";
+console.log(a == c);
+console.log(a === c);
+
+const numbers = [4, 9, 16, 25, 44];
+let info = numbers.map(myFun);
+
+function myFun(value) {
+  console.log("here is the value of array: " + value * 12);
+}
+// set, weekMap and workSet
+let myArray = new Set(numbers);
+console.log(myArray);
+myArray.add(12);
+myArray.delete(12);
+
+//try block of code
+try {
+} catch (err) {}
+//JavaScript Throws Errors
+//JavaScript will actually create an Error object with two properties: name and message.
+
+//The throw Statement
+//The exception can be a JavaScript String, a Number, a Boolean or an Object:
+//If you use throw together with try and catch, you can control program flow and generate custom error messages.
+try {
+  // Code that may throw an exception
+  const result = someUndefinedVariable / 0;
+  console.log(result); // This line will not be executed due to the exception
+} catch (error) {
+  // Handle the exception
+  console.error("An error occurred:", error.message);
+}
+
+try {
+  let m = 700;
+  const result = m / 12;
+  console.log(result);
+} catch (err) {
+  console.log("An error message:", err.message);
+}
+
+const ab = new WeakSet();
+// const cd = [];
+
+function abc() {
+  const item = {};
+  // cd.push(item);
+  // const r = 4;
+  // console.log(r);
+  ab.add(item); 
+}
+abc();
