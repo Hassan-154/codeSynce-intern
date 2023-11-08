146f67b9f4d97ae197373d2086431a649bce1e75
Hassan-154 <83923230+Hassan-154@users.noreply.github.com>
2023-11-08 13:28:15 +0500

    final update

---
+++ script8.js
@@ -0,0 +1,17 @@
+console.log("string method: ");
+
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+console.log( 'total length: ' + text.length)
+
+//Extracting string path
+
+//slice(start, end)
+//substring(start, end)
+//substr(start, length)
+
+let str = "Apple, Banana, Kiwi";
+console.log(str)
+
+
+
+
